161a56c4afc96c2eda9f0f417dff7da23f723bfd
tysonpace777 <32.tpace@gmail.com>
2026-03-04 12:38:48 -0700

    java hello world

---
+++ src/main/java/org/tysonpace/javaproject/JavaProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.tysonpace.javaproject;
+
+/**
+ *
+ * @author 32tpa
+ */
+public class JavaProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
