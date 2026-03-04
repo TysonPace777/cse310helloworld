b6d3ea7887baaa86d7b6b7919c63ed194b23e4e3
tysonpace777 <32.tpace@gmail.com>
2026-03-04 12:52:56 -0700

    duplicate file

--- helloworld/src/main/java/tyson/helloworld/Helloworld.java
+++ helloworld/src/main/java/tyson/helloworld/Helloworld.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package tyson.helloworld;
 
-/**
- *
- * @author 32tpa
- */
 public class Helloworld {
 
     public static void main(String[] args) {
